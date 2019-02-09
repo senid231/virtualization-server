3fdb72c4f867024ea70fe20f85e7840a0310499b
Brooks Swinnerton <bswinnerton@gmail.com>
2019-02-09 14:11:21 -0500

    Return data from list vms route

--- app/routes/virtual_machines.rb
+++ app/routes/virtual_machines.rb
@@ -1,9 +1,33 @@
 module NeptuneNetworks::Virtualization
   module Routes
     class VirtualMachines < Sinatra::Base
+      before do
+        content_type :json
+      end
+
+      # https://libvirt.org/html/libvirt-libvirt-domain.html#virDomainState
+      STATES = {
+				0 => "no state",
+				1 => "running",
+				2 => "blocked on resource",
+				3 => "paused by user",
+				4 => "being shut down",
+				5 => "shut off",
+				6 => "crashed",
+				7 => "suspended by guest power management",
+			}
+
       # List all virtual machines
       get '/virtual_machines' do
-        libvirt.list_domains
+        domains = libvirt.list_all_domains
+        resp = domains.map do |domain|
+          {
+            name: domain.name,
+            state: STATES[domain.state.first],
+            cpus: domain.max_vcpus,
+            memory: domain.max_memory,
+          }.to_json
+        end
       end
 
       # Show a single virtual machine
