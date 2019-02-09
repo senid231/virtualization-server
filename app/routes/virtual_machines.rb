f312ab9e321d132b9c6584c6a8bdd30e66255356
Brooks Swinnerton <bswinnerton@gmail.com>
2019-02-09 12:46:43 -0500

    Add shell of all virtual machine endpoints

--- app/routes/virtual_machines.rb
+++ app/routes/virtual_machines.rb
@@ -1,8 +1,29 @@
 module NeptuneNetworks::Virtualization
   module Routes
     class VirtualMachines < Sinatra::Base
-      get '/' do
-        puts 'foo'
+      # List all virtual machines
+      get '/virtual_machines' do
+        #TODO
+      end
+
+      # Show a single virtual machine
+      get '/virtual_machines/:guid' do
+        #TODO
+      end
+
+      # Create a new virtual machine
+      post '/virtual_machines/:guid' do
+        #TODO
+      end
+
+      # Update an existing virtual machine
+      put '/virtual_machines/:guid' do
+        #TODO
+      end
+
+      # Delete a virtual machine
+      delete '/virtual_machines/:guid' do
+        #TODO
       end
     end
   end
