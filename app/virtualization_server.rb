d78177f0793b1efcead5dd0436047d38cddd25c6
Denis Talakevich <senid231@gmail.com>
2019-12-27 16:23:23 +0200

    fail when config is not present

--- app/virtualization_server.rb
+++ app/virtualization_server.rb
@@ -1,12 +1,31 @@
 module VirtualizationServer
   class Config < Anyway::Config
     config_name :app # set load path config/app.yml
+
     attr_config :cookie_secret,
                 :libvirt_domain_type,
                 :clusters,
                 :users,
                 :root,
                 libvirt_rw: false
+
+    def load!
+      raise RuntimeError, "Config #{config_path} does not exist" unless File.file?(config_path)
+      load # will load config/app.yml
+      validate_presence! :users, :clusters, :cookie_secret
+    end
+
+    private
+
+    def validate_presence!(*names)
+      names.each do |name|
+        raise RuntimeError, "Key #{name} must be present at #{config_path}" if public_send(name).nil?
+      end
+    end
+
+    def config_path
+      @config_path ||= default_config_path(config_name)
+    end
   end
 
   def environment
--- config/initializers/_config.rb
+++ config/initializers/_config.rb
@@ -1,3 +1,3 @@
-VirtualizationServer.config.load # will load config/app.yml
+VirtualizationServer.config.load! # will load config/app.yml or raise if it does not exist
 
 User.load_storage VirtualizationServer.config.users
